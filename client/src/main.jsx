76fd27932c3b683ce2272c99b6268f216d8f2393
antiennn <antien2k3@gmail.com>
2025-03-02 02:03:06 +0700

    feat: add speed Insights

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
-
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { StateContextProvider } from './context';
 import App from './App';
 import './index.css';
@@ -14,6 +14,7 @@ root.render(
     <Router>
       <StateContextProvider>
         <App />
+        <SpeedInsights />
       </StateContextProvider>
     </Router>
   </ThirdwebProvider> 
