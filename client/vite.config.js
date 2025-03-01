88d0ad061b09230113f3b2d946ec961938aa84e7
antiennn <antien2k3@gmail.com>
2025-03-02 01:30:57 +0700

    fix: fix vite config to deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: "globalThis",
-    "process.env": {},
-  },
   build: {
     target: "esnext",
   },
